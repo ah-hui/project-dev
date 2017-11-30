6ed49f496fe27ecc909f639b69650a1bed425a88
刘光辉 <yuzhong@suneee.com>
2017-11-30 15:49:35 +0800

    [INIT] 初始化/引入spring-boot

---
+++ src/main/java/ind/lgh/ProjectDevApplication.java
@@ -0,0 +1,15 @@
+package ind.lgh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lgh
+ */
+@SpringBootApplication
+public class ProjectDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDevApplication.class, args);
+	}
+}
---
+++ src/main/java/ind/lgh/ServletInitializer.java
@@ -0,0 +1,16 @@
+package ind.lgh;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * @author lgh
+ */
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectDevApplication.class);
+	}
+
+}
