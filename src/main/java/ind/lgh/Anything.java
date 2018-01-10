66916af5b000b7c15edc9c0cba62b4f173796d71
刘光辉 <yuzhong@suneee.com>
2018-01-10 11:07:22 +0800

    [ADD] 工作相关，在anything.java中实现统计目录下文件的行数功能

--- src/main/java/ind/lgh/Anything.java
+++ src/main/java/ind/lgh/Anything.java
@@ -1,68 +1,59 @@
 package ind.lgh;
 
 
-import java.util.concurrent.*;
+import java.io.*;
+import java.util.HashSet;
 
 /**
- * 在这里随便玩
+ * 工作相关：
+ * 读取指定目录下的全部文件（非递归）并计算每个文件的行数
  *
  * @author lgh
  * @since 2017-12-21
  */
 public class Anything {
 
+//    public static final String FILE_DIR = "C:\\Users\\BJQT\\Desktop\\项目代码检查\\20180105总结\\javafile";
+
+    public static final String FILE_DIR = "C:\\Users\\BJQT\\Desktop\\项目代码检查\\20180104 - 4\\循环场项目组代码检查\\抽查";
+
+    public static HashSet<String> ALL_FILE = new HashSet<String>();
+
     public static void main(String[] args) {
-        multiThreadTest();
+        getAllFileName();
+        System.out.println("文件数量：" + ALL_FILE.size());
+        for (String fileName : ALL_FILE) {
+            try {
+                getFileLineCount(fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void getFileLineCount(String fileName) throws IOException {
+        LineNumberReader reader = new LineNumberReader(new FileReader(fileName));
+        String lineRead = "";
+        while ((lineRead = reader.readLine()) != null) {
+        }
+        System.out.println(fileName + "---" + reader.getLineNumber());
     }
 
     /**
-     * 多线程测试
-     * 1.Future接口就是对于具体的Runnable或者Callable任务的执行结果进行取消、查询是否完成、获取结果。
-     * 2.必要时可以通过get方法获取执行结果，该方法会阻塞直到任务返回结果。
-     * 3.与FutureTask区别：后者是前者的唯一实现类
+     * 非递归方式读取文件
      */
-    public static void multiThreadTest() {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        MyTask task = new MyTask();
-        Future<Integer> result = executorService.submit(task);
-        executorService.shutdown();
-
-        System.out.println("=== 主线程开始执行其他任务 ===");
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public static void getAllFileName() {
+        File f = new File(FILE_DIR);
+        if (!f.exists()) {
+            System.out.println("目录不存在！" + FILE_DIR);
+            return;
         }
-        System.out.println("=== 主线程执行其他任务完毕 ===");
-
-        try {
-            System.out.println("=== task运行结果：" + result.get() + " ===");
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        File[] arr = f.listFiles();
+        for (int i = 0; i < arr.length; i++) {
+            File file = arr[i];
+            if (!file.isDirectory()) {
+                ALL_FILE.add(FILE_DIR + "\\" + file.getName());
+            }
         }
-
-        System.out.println("=== 所有任务执行完毕 ===");
-
-    }
-}
-
-/**
- * 多线程任务类
- * <p>
- * 关于同级类：
- * 1.一个.java文件中可以有多个同级类
- * 2.其修饰符只可以public/abstract/final和无修饰符，不能是其他的protected/private等修饰符
- * 3.不使用任何修饰符声明的属性和方法，对同一个包内的类是可见的
- * 4.public修饰的只能有一个,且必须要与文件名相同
- * 5.编译时一个java文件会生成多个class文件
- */
-class MyTask implements Callable<Integer> {
-    @Override
-    public Integer call() throws Exception {
-        System.out.println("=== 子线程执行 ===");
-        Thread.sleep(3000);
-        return 100;
     }
 }
